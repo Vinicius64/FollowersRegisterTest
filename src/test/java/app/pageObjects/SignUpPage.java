cfc6a1537a73579478ff1f99ad48f19b4639409b
adriann postigo <adriannparanhos@gmail.com>
2024-12-03 20:40:58 -0300

    Feat: Add WebDriver and constructor

--- src/test/java/app/pageObjects/SignUpPage.java
+++ src/test/java/app/pageObjects/SignUpPage.java
@@ -1,5 +1,14 @@
 package app.pageObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class SignUpPage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public SignUpPage(WebDriver driver) {
+        this.driver = driver;
+    }
 
 }
