d07cf6e732867ff91e637ad53c0bc2ed44480d75
adriann postigo <adriannparanhos@gmail.com>
2024-12-03 20:49:46 -0300

    Feat: Add method to wait for ok button

--- src/test/java/app/pageObjects/SignUpPage.java
+++ src/test/java/app/pageObjects/SignUpPage.java
@@ -1,7 +1,11 @@
 package app.pageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class SignUpPage {
     private WebDriver driver;
@@ -15,5 +19,13 @@ public class SignUpPage {
         driver.get("https://followers-register.vercel.app");
     }
 
+    private By okButton = By.cssSelector(".swal2-actions button");
 
+
+    public void clickOkButton() { driver.findElement(okButton).click();}
+
+    public void waitForOkButtonToBeVisible() {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(okButton));
+    }
 }
