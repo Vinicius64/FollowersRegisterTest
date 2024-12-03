1989cbf9bae99f04ed0a3f348cf7bae44f3e5930
Vinicius64 <oliveira.vinicius64@gmail.com>
2024-12-03 20:59:13 -0300

    feat(SignUpFollowersTest): create method setUp to configure initial page

--- src/test/java/followers/signUpFollowers/SignUpFollowersTest.java
+++ src/test/java/followers/signUpFollowers/SignUpFollowersTest.java
@@ -2,10 +2,14 @@ package followers.signUpFollowers;
 
 import app.pageObjects.SignUpPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.opentest4j.AssertionFailedError;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("Testing the functionality of registering a follower")
 public class SignUpFollowersTest {
@@ -17,5 +21,6 @@ public class SignUpFollowersTest {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         signUpPage = new SignUpPage(driver);
+        signUpPage.open();
     }
 }
