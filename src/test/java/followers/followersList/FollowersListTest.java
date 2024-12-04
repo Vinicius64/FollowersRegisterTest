898e99041f2ae2cb47167129c1e8a1aea5fb69b9
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-12-04 01:46:36 -0300

    feat: create a simple test to insert in signup page

--- src/test/java/followers/followersList/FollowersListTest.java
+++ src/test/java/followers/followersList/FollowersListTest.java
@@ -2,10 +2,15 @@ package followers.followersList;
 
 import app.pageObjects.SignUpPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.opentest4j.AssertionFailedError;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FollowersListTest {
     private static WebDriver driver;
@@ -18,4 +23,42 @@ public class FollowersListTest {
         driver = new ChromeDriver();
         signUpPage = new SignUpPage(driver);
     }
+
+    @Order(1)
+    @ParameterizedTest
+    @DisplayName("Test simple insert")
+    @CsvSource({
+            "John Doe, Male, 100, true",
+            "Jane Smith, Female, 200, true"
+    })
+    public void testSimpleInsert(String name, String gender, String nivel, boolean expected){
+        try{
+            signUpPage.open();
+            insertData(name, gender, nivel);
+            signUpPage.waitForOkButtonToBeVisible();
+            assertEquals(expected, signUpPage.isRegisterSuccessMessageVisible());
+            clickOkButton();
+        }catch (Error e){
+            clickOkButton();
+            throw new AssertionFailedError("The result is not the expected");
+        }
+    }
+
+    private void insertData(String name, String gender, String nivel) {
+        signUpPage.fillName(name);
+        signUpPage.selectGender(gender);
+        signUpPage.fillNivel(nivel);
+        signUpPage.selectFirstRadioOption();
+        signUpPage.selectSelectedImageOption();
+        signUpPage.clickRegisterButton();
+    }
+
+    private void clickOkButton() {
+        signUpPage.waitForOkButtonToBeVisible();
+        signUpPage.clickOkButton();
+        signUpPage.waitForRegisterTitleToBeVisible();
+        signUpPage.scrollToTop();
+        signUpPage.reloadPage();
+    }
+
 }
