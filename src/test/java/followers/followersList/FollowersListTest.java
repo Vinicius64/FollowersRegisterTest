cd493d582ac9ce86afcb5f5fc67421661070da20
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-12-04 01:34:18 -0300

    feat: create followers list test

---
+++ src/test/java/followers/followersList/FollowersListTest.java
@@ -0,0 +1,11 @@
+package followers.followersList;
+
+import app.pageObjects.SignUpPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class FollowersListTest {
+    private static WebDriver webDriver;
+    private static SignUpPage signUpPage;
+    private WebDriverWait wait;
+}
