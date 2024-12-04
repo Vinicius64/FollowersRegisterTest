ad92979e827ea47998d4bb0380d527a4a629e8a6
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-12-04 01:35:28 -0300

    feat: add before all method in followers list test

--- src/test/java/followers/followersList/FollowersListTest.java
+++ src/test/java/followers/followersList/FollowersListTest.java
@@ -1,11 +1,21 @@
 package followers.followersList;
 
 import app.pageObjects.SignUpPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class FollowersListTest {
-    private static WebDriver webDriver;
+    private static WebDriver driver;
     private static SignUpPage signUpPage;
     private WebDriverWait wait;
+
+    @BeforeAll
+    public static void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        signUpPage = new SignUpPage(driver);
+    }
 }
